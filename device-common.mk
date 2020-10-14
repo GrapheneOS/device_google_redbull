20dbd34428621704448e09c727d27e81888dee25
Daniel Micay <danielmicay@gmail.com>
2020-10-14 19:17:50 -0400

    disable GSI keys

--- device-common.mk
+++ device-common.mk
@@ -90,9 +90,6 @@ TARGET_PRODUCT_PROP := $(LOCAL_PATH)/product.prop
 
 $(call inherit-product, $(LOCAL_PATH)/utils.mk)
 
-# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
-
 PRODUCT_CHARACTERISTICS := nosdcard
 PRODUCT_SHIPPING_API_LEVEL := 30
 
