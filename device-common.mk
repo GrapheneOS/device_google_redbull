e40f4b68d6d37c8a6c3fcddfe556ce47014a5eee
flawedworld <flawedworld@flawed.world>
2022-04-18 20:24:48 +0100

    Use inline carrier extraction

--- device-common.mk
+++ device-common.mk
@@ -38,7 +38,8 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/google/interfaces \
     vendor/google_devices/common/proprietary/confirmatioui_hal \
     vendor/google_nos/host/android \
-    vendor/google_nos/test/system-test-harness
+    vendor/google_nos/test/system-test-harness \
+    vendor/carriersettings-extractor
 
 # Include GPS soong namespace
 PRODUCT_SOONG_NAMESPACES += \
@@ -991,6 +992,10 @@ PRODUCT_VENDOR_PROPERTIES += ro.soc.model=SM7250
 PRODUCT_PACKAGES += \
     PresencePolling \
     RcsService
+
+PRODUCT_PACKAGES += \
+    extracted-carrierconfig \
+    extracted-apns
 
 #################################################################################
 # This is the End of device-common.mk file.
