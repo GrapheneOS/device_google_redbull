4d05fa30eded5a781bf0797256d339cfc954ef08
Steve Berbary <sberbary@google.com>
2023-03-15 02:11:05 +0000

    Update redbull SVN to 66
    Bug: 272844037
    Change-Id: I4be916b592c8251f780f48541df718f8830a0bc8

--- device-common.mk
+++ device-common.mk
@@ -845,7 +845,7 @@ PRODUCT_PACKAGES += $(HIDL_WRAPPER)
 # Increment the SVN for any official public releases
 ifeq ($(PRODUCT_DEVICE_SVN_OVERRIDE),)
 PRODUCT_PROPERTY_OVERRIDES += \
-	ro.vendor.build.svn=65
+	ro.vendor.build.svn=66
 endif
 
 # Enable iwlan service logging for debug
